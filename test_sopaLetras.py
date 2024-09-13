0636dfc02f2426369456d57abb9b5f0365944a93
valriosben <valeria.rios@palmetto.com>
2024-09-12 22:26:50 -0300

    Agrego un test unitario

--- test_sopaLetras.py
+++ test_sopaLetras.py
@@ -1,10 +1,12 @@
 # test_sopaLetras.py
 import unittest
 from unittest.mock import patch
-from sopaLetras import pedirRespuesta, elegirCategoria, elegirDificultad, iniciarJuego
+from sopaLetras import pedirRespuesta, elegirCategoria, elegirDificultad, iniciarJuego, crearMatriz, categoriaMatriz
 
 class TestSopaLetras(unittest.TestCase):
 
+
+# Patch simula la entrada del usuario
     @patch('builtins.input', side_effect=['Y'])
     def test_iniciarJuego_continuar(self, mock_input):
         categoria, dificultad = iniciarJuego()
@@ -26,6 +28,15 @@ class TestSopaLetras(unittest.TestCase):
     def test_elegirDificultad(self, mock_input):
         dificultad = elegirDificultad()
         self.assertEqual(dificultad, 'F')
+
+# En las siguientes funciones ya no ingresa directamente un valor, por lo cual 
+# la función ya recibe argumentos
+# El self es un método que accede a los atributos y métodos de la instancia actual  
+    def test_crearMatriz(self):
+        matriz = crearMatriz('F')
+        esperado = [["*" for _ in range(8)] for _ in range(8)]
+        self.assertEqual(matriz, esperado)
+    
 
 if __name__ == '__main__':
     unittest.main()
